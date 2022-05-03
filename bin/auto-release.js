d16dc01209d584a8124ff1a87f132c33458d85bd
黄宝成 <huangbc@publink.cn>
2022-05-03 21:48:39 +0800

    release: 0.0.5

--- bin/auto-release.js
+++ bin/auto-release.js
@@ -15,7 +15,7 @@ if (isNotUndefined(argv.init)) {
 }
 
 if (isNotUndefined(argv.version)) {
-  console.log('0.0.4')
+  console.log('0.0.5')
   return
 }
 else if(isNotUndefined(argv.ci) || isNotUndefined(argv.CI)) {
